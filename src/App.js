f3efedb2f9d21c91aa01699e41ba63232219e193
Dorus ten Haaf <dthaaf@wundermancommerce.com>
2021-11-02 16:50:05 +0100

    added plp route and page start

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
+import ProductListPage from './pages/ProductListPage';
 import Error from "./pages/Error";
 
 import {Route, Switch} from 'react-router-dom';
@@ -17,6 +18,7 @@ function App() {
 			<Route exact path="/" component={Home}/>
 			<Route exact path="/rooms/" component={Rooms}/>
 			<Route exact path="/rooms/:slug" component={SingleRoom}/>
+			<Route exact path="/products" component={ProductListPage}/>
 			<Route component={Error}/>
 		</Switch>
 		
---
+++ src/components/plp/filters/Filters.js
@@ -0,0 +1,9 @@
+const ProductFilters = () => {
+	return (
+		<section className="plp-filters">
+            <p>product filters</p>
+        </section>
+	);
+}
+
+export default ProductFilters; 
