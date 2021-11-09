aa96e4b16c6b9607d0822ef98c8d3cf00abd220f
Your Name <you@example.com>
2021-11-09 15:47:47 +0100

    POC-data removed room components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.scss';
 
 import Home from "./pages/Home";
-import Rooms from "./pages/Rooms";
-import SingleRoom from "./pages/SingleRoom";
 import ProductListPage from './pages/ProductListPage';
 import Error from "./pages/Error";
 
@@ -17,8 +15,6 @@ function App() {
 		<Navbar />
 		<Switch>
 			<Route exact path="/" component={Home}/>
-			<Route exact path="/rooms/" component={Rooms}/>
-			<Route exact path="/rooms/:slug" component={SingleRoom}/>
 			<Route exact path="/products" component={ProductListPage}/>
 			<Route component={Error}/>
 		</Switch>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,17 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
-import Services from '../components/Services';
-import FeaturedRooms from '../components/FeaturedRooms';
 
 const Home = () => {
 	return (
 		<>
 			<Hero>
-				<Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+				<Banner title="POC Products" subtitle="POC products starting at $299">
 					<Link to='/rooms' className="btn-primary">
-						our rooms
+						our products
 					</Link>
 				</Banner>
 			</Hero>
-			<Services />
-			<FeaturedRooms />
-			
 		</>
 	);
 }
