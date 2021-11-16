f9cb9a161bcc9c5247dfa20425d3436f73a5735e
Dorus ten Haaf <dthaaf@wundermancommerce.com>
2021-11-16 13:23:07 +0100

    added filter controlls

--- src/components/plp/filters/ProductFilters.js
+++ src/components/plp/filters/ProductFilters.js
@@ -1,14 +1,50 @@
+import { useState } from "react";
 import ActiveFilters from "./components/ActiveFilters";
+import Filter from "./components/Filter";
 import SubCategories from "./components/SubCategories";
 
 const ProductFilters = () => {
-	let subCategories = ["Chromebooks", "Ultrabooks", "Apple Macbooks", "Accessoires"]
-	let activeFilters = [{name:"used"}, {name:"test"}]
+	const [activeFilters, setactiveFilters] = useState([{filterName:"condition", value:"used"},{filterName:"check", value:"1212"}])
+	const [subCategories, setsubCategories] = useState(["Chromebooks", "Ultrabooks", "Apple Macbooks", "Accessoires"])
+	let filters = [
+		{
+			filterName:"condition",
+			values:["New", "used", "Renewed", "Refurbished", "Showroom sample"],
+			type:"radio"
+		}
+	]
+
+	let handleRemove = (val1, val2)=> {
+		let output = activeFilters.filter((val) => val.filterName!==val2&&val.value!==val1)
+		setactiveFilters(output)
+	}
+
+	let handleAdd = (val1, val2, val3)=> {
+		let output = {filterName:val2, value:val1}
+		if(val3==="radio"){
+			let able = true
+			activeFilters.map(act=>{
+				if(act.filterName===val2){
+					able = false
+				}
+			})
+			if(able===true){
+				setactiveFilters([...activeFilters, output])
+			}
+		}else{
+			setactiveFilters([...activeFilters, output])
+		}
+	}
 
 	return (
 		<section className="plp-filtercontainer">
 			{ subCategories &&<SubCategories categories={subCategories}/>}
 			{ activeFilters &&<ActiveFilters activeFilters={activeFilters}/>}
+			{filters.map((filter, key)=>{
+				return(
+					<Filter key={key} activeFilters={activeFilters} handleRemove={handleRemove} handleAdd={handleAdd}  filter={filter}/>
+				)
+			})}
         </section>
 	);
 }
