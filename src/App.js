81feab5cd1ceb48a8daadb7834c5c2628f744478
Your Name <you@example.com>
2021-11-16 15:12:34 +0100

    POC-data product data from content

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends React.Component {
 	}
 
 	render() {
-		console.log('stat= ' + this.state.products)
+		console.log(this.state.products)
 		return (
 			<>
 			<Navbar />
--- src/components/Products.js
+++ src/components/Products.js
@@ -2,24 +2,16 @@ import React from 'react'
 import Product from './Product'
 
 const products = ( props ) => {
-	console.log('product= ' + props)
+	console.log(props)
 	return (
 		<div>
 			{props.products && props.products.map(
 				(product, index) =>{
-					console.log(product);
 					return <Product product={product} key={index} />
 				}
 			)}
-
-
-			
 		</div>
 	)
-}
-
-products.propTypes = {
-
 }
 
 export default products
