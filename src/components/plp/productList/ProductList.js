3f898179377b7b3743df9a64749fd45255d3be60
Anton Kritikos <anton.kritikos@wundermanthompson.com>
2021-11-25 10:34:16 +0100

    Added list of products to lister page

--- src/components/plp/productList/ProductList.js
+++ src/components/plp/productList/ProductList.js
@@ -1,9 +1,31 @@
-const ProductList = () => {
-	return (
-		<section className="plp-productlist">
-            <p>productlist</p>
-        </section>
-	);
+import React from 'react'
+import { client } from '../../../client';
+import Products from '../../../components/Products'
+
+class ProductList extends React.Component {
+	state = {
+		products: []
+	}
+	componentDidMount() {
+		client.getEntries()
+		.then((response) => {
+			console.log(response)
+			this.setState({
+				products: response.items
+			})
+		})
+		.catch(console.error)
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="wrapper">
+					{this.state.products && <Products products={this.state.products} />}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default ProductList; 
